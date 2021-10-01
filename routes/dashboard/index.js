eb84d01860d189a3f80fbc338523b69f07dbfc7f
Tiago Danin <TiagoDanin@outlook.com>
2021-09-30 22:46:30 -0300

    fix: no save where no exist item

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -4,21 +4,22 @@ const router = express.Router();
 
 const Paper = require('../../models/paper');
 
+const result = []
+
 const dashboard = async (req, res) => {
 	const id = req.body?.id
-	let result = []
 
 	if (id) {
 		const paper = new Paper(id)
 
 		const response = await fetch(`https://www.okanebox.com.br/api/acoes/ultima/${paper.getId}/`)
 		const data = await response.json()
-
-		paper.setOpenPrice(data.PREABE)
-		paper.setClosePrice(data.PREULT)
-		paper.setDate(data.DATPRG)
-	
-		result.push(paper)
+		if (!Array.isArray(data)) {
+			paper.setOpenPrice(data.PREABE)
+			paper.setClosePrice(data.PREULT)
+			paper.setDate(data.DATPRG)
+			result.push(paper)
+		}
 	}
 
 	return res.render('dashboard', {
