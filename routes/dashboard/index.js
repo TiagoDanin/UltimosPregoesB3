163b7a3b86fecd380fda2a03ff857d59afc5f7b1
Tiago Danin <TiagoDanin@outlook.com>
2021-10-01 15:36:40 -0300

    fix: show date

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -1,16 +1,32 @@
 const express = require('express')
+const jsonfile = require('jsonfile')
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const router = express.Router();
 
-const Paper = require('../../models/paper');
+const file = 'data.json'
+const ids = jsonfile.readFileSync(file)
 
-const result = []
+const save = () => {
+	jsonfile.writeFileSync(file, ids, {
+		replacer: true,
+		spaces: '\t'
+	})
+}
+
+const Paper = require('../../models/paper');
 
 const dashboard = async (req, res) => {
 	const id = req.body?.id
 
+	const result = []
+
 	if (id) {
-		const paper = new Paper(id)
+		ids.push(id)
+		save()
+	}
+
+	for (idPaper of ids) {
+		const paper = new Paper(idPaper)
 
 		const response = await fetch(`https://www.okanebox.com.br/api/acoes/ultima/${paper.getId}/`)
 		const data = await response.json()
